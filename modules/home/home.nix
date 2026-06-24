a35ead93a72e8a4a7fbd21b34d39c59fed11b71f
Fernleafie <fernleafie@gmail.com>
2026-06-24 18:11:30 +0200

    Formatting and missed tooling import

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -7,23 +7,27 @@
       ./programs # terminal applications
       ./shell # shell options and replacement terminal commands
       ./system # wm, de, and or shell
+      ./tooling # programming language specific configurations
+
       ./catppuccin.nix
     ];
 
-  xdg.enable = true;
-  xdg.mime.enable = true;
-  xdg.mimeApps.enable = true;
-  xdg.userDirs = {
+  xdg = {
     enable = true;
-    createDirectories = false;
-    desktop = "${config.home.homeDirectory}/desktop";
-    documents = "${config.home.homeDirectory}/documents";
-    download = "${config.home.homeDirectory}/downloads";
-    music = "${config.home.homeDirectory}/music";
-    pictures = "${config.home.homeDirectory}/pictures";
-    projects = "${config.home.homeDirectory}/projects";
-    videos = "${config.home.homeDirectory}/videos";
-    setSessionVariables = true;
+    mime.enable = true;
+    mimeApps.enable = true;
+    userDirs = {
+      enable = true;
+      createDirectories = false;
+      desktop = "${config.home.homeDirectory}/desktop";
+      documents = "${config.home.homeDirectory}/documents";
+      download = "${config.home.homeDirectory}/downloads";
+      music = "${config.home.homeDirectory}/music";
+      pictures = "${config.home.homeDirectory}/pictures";
+      projects = "${config.home.homeDirectory}/projects";
+      videos = "${config.home.homeDirectory}/videos";
+      setSessionVariables = true;
+    };
   };
   programs.git = {
     enable = true;
