d0558fe8c2868f09e85fda8c1ecd5e772522d1c5
Fernleafie <fernleafie@gmail.com>
2026-07-15 12:28:17 +0200

    apps/rmpc: fix mpd audio outputs
    u can now adjust the volume of mpd!

--- modules/home/apps/base/rmpc.nix
+++ modules/home/apps/base/rmpc.nix
@@ -16,10 +16,26 @@
 
   home.packages = [
     inputs.rmpc.packages.${pkgs.stdenv.hostPlatform.system}.rmpcd
+    pkgs.playerctl
   ];
   xdg.configFile."rmpcd/init.lua".text = builtins.readFile ./rmpcd-init.lua;
 
   services = {
-    mpd.enable = true;
+    playerctld.enable = true;
+    mpd = {
+      enable = true;
+      extraConfig = ''
+        audio_output {
+          type "pipewire"
+          name "PipeWire Sound Server"
+        }
+        audio_output {
+          type "fifo"
+          name "my_fifo"
+          path "/tmp/mpd.fifo"
+          format "44100:16:2"
+        }
+      '';
+    };
   };
 }
