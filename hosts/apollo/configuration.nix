93277c4a381e6b397f5b4e3a58954a29bd7af102
Kyoko <129420497+CodedKyoko@users/noreply.github.com>
2026-08-09 15:05:10 +0100

    added apollo host

---
+++ hosts/apollo/configuration.nix
@@ -0,0 +1,43 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./../../modules/nixos
+    ./../../modules/shared
+  ];
+
+  snow = {
+    programs.firefox.extraExtensions = with pkgs.nur.repos.rycee.firefox-addons; [
+      scriptcat
+    ];
+    gaming.enable = true;
+    graphical.enable = true;
+    writing.enable = true;
+    tooling = {
+      typst.enable = true;
+      nix.enable = true;
+      rust.enable = false;
+    };
+    user-services.enable = true;
+  };
+
+  # Define your hostname and location
+  networking.hostName = "apollo";
+  time.timeZone = "Europe/London";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’
+  users.users.lily-snowleafie = {
+    isNormalUser = true;
+    description = "Lily Snowleafie";
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+    ]; # Enable ‘sudo’ for the user.
+    shell = pkgs.fish;
+  };
+}
